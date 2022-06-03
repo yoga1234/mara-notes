1da7847234cecb9e0ec77c2b12115910cdce5f68
Septiana Yoga <septianayoga30@gmail.com>
2022-06-03 14:13:59 +0700

    adding rightside basic styling

--- src/components/rightSide/RightSide.js
+++ src/components/rightSide/RightSide.js
@@ -2,7 +2,23 @@ import React from "react";
 import "./RightSide.css";
 
 const RightSide = () => {
-  return <div className="right-container">RightSide</div>;
+  return (
+    <div className="right-container">
+      <p className="note-source">
+        From <span>Archive</span>
+      </p>
+      <div className="note-container">
+        <h3 className="right-note-title">Title goes here</h3>
+        <p className="right-note-content">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+          saepe repudiandae dolores cupiditate incidunt eaque omnis iste nulla
+          odit assumenda. Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Fugiat laudantium quia maiores aperiam vitae ad adipisci nemo
+          cupiditate laboriosam omnis!
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default RightSide;
